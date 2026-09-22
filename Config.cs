77ea5c84f8961c4177d8f13dfaaa5f0da94e7561
SoulMinf <73591105+SoulMinf@users.noreply.github.com>
2026-09-23 00:09:07 +0300

    v1.2 round sliders

--- Config.cs
+++ Config.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System;
 using Terraria.ModLoader.Config;
 
 namespace SethCameraFocus
@@ -6,6 +7,7 @@ namespace SethCameraFocus
     public class Config : ModConfig
     {
         public override ConfigScope Mode => ConfigScope.ClientSide;
+        private float _DetachDistanceTiles = 100f;
 
         [Header("$Mods.SethCameraFocus.EnableGroups")]
 
@@ -19,24 +21,43 @@ namespace SethCameraFocus
 
         [Header("$Mods.SethCameraFocus.Tuning")]
 
+        
         [LabelKey("$Mods.SethCameraFocus.MaxRange")]
         [Range(10f, 200f)]
         [DefaultValue(100f)]
-        public float DetachDistanceTiles = 100f;
+        public float DetachDistanceTiles
+        {
+            get => (float)Math.Round(_DetachDistanceTiles, 0);
+            set => _DetachDistanceTiles = value;
+        }
 
+        private float _MaxParallax = 0.35f;
         [LabelKey("$Mods.SethCameraFocus.ParalaxStrength")]
         [Range(0.05f, 0.8f)]
         [DefaultValue(0.35f)]
-        public float MaxParallax = 0.35f;
+        public float MaxParallax
+        {
+            get => (float)Math.Round(_MaxParallax, 2);
+            set => _MaxParallax = value;
+        }
 
+        private float _SpeedIn = 0.04f;
         [LabelKey("$Mods.SethCameraFocus.ParalaxIn")]
         [Range(0.01f, 0.2f)]
         [DefaultValue(0.04f)]
-        public float SpeedIn = 0.04f;
+        public float SpeedIn
+        {
+            get => (float)Math.Round(_SpeedIn, 2);
+            set => _SpeedIn = value;
+        }
 
+        private float _SpeedOut = 0.02f;
         [LabelKey("$Mods.SethCameraFocus.ParalaxOut")]
         [Range(0.01f, 0.2f)]
-        [DefaultValue(0.025f)]
-        public float SpeedOut = 0.025f;
+        [DefaultValue(0.02f)]
+        public float SpeedOut {
+            get => (float)Math.Round(_SpeedOut, 2);
+            set => _SpeedOut = value;
+        }
     }
 }
